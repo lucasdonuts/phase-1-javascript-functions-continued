28754bf95a8d58cd014865ff2ed8bf1d49719a9b
Lucas Duncan <lucasdonuts@gmail.com>
2022-06-27 15:10:45 -0500

    Was not a test error, just a user error. All tests passing

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ function mondayWork(activity = 'go to the office') {
   return `This Monday, I will ${activity}.`;
 }
 
-function wrapAdjective(char, adj) {
-  return `You are ${char} ${adj} ${char}!`;
+function wrapAdjective(char) {
+  const innerFunction = function (adj) {
+
+    return `You are ${char}${adj}${char}!`;
+
+  }
+
+  return innerFunction;
 }
